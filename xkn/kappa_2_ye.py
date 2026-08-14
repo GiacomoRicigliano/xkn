ad1617eaaf97e946ec2b14d0245384e08d14bd00
ghuez <giulia.huez@uni-jena.de>
2026-08-14 14:12:35 +0200

    fix Wu opacities

--- xkn/kappa_2_ye.py
+++ xkn/kappa_2_ye.py
@@ -2,6 +2,13 @@ import numpy as np
 import sys
 from scipy.interpolate import RegularGridInterpolator, interp1d
 
+def func_wu_opacities(ye, kappa_min=1, kappa_up=9):
+    kappa = kappa_min + kappa_up / (1 + (4 * ye)** 12)
+    return kappa
+
+def func_wu_opacities_invert(kappa, kappa_min=1, kappa_up=9):
+    ye = ((kappa_min + kappa_up - kappa) / (kappa - kappa_min))**(1/12)/4
+    return ye
 
 class Kappa2Ye(object):
 
@@ -10,8 +17,8 @@ class Kappa2Ye(object):
             self.f_ye2k = func_interp_tanaka()
             self.f_k2ye = func_interp_tanaka_invert()
         elif ye_k_dep == "Wu":
-            self.f_ye2k = func_wu_opacities()
-            self.f_k2ye = func_wu_opacities_invert()
+            self.f_ye2k = func_wu_opacities
+            self.f_k2ye = func_wu_opacities_invert
         else:
             sys.exit(
                 "Wrong input name for ye-k dependence\n"
@@ -38,15 +45,3 @@ def func_interp_tanaka_invert():
     ye_tan = np.asarray([0.01, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.50])[::-1]
     k_tan = np.asarray([30.1, 30.0, 29.9, 22.30, 5.60, 5.36, 3.30, 0.96, 0.1])[::-1]
     return interp1d(k_tan, ye_tan, kind="linear", fill_value="extrapolate")
-
-def func_wu_opacities():
-    def ye2k(ye, kappa_min=1, kappa_up=9):
-        kappa = kappa_min + kappa_up / (1 + (4 * ye)** 12)
-        return kappa
-    return ye2k
-
-def func_wu_opacities_invert():
-    def k2ye(kappa, kappa_min=1, kappa_up=9):
-        ye = ((kappa_min + kappa_up - kappa) / (kappa - kappa_min))**(1/12)/4
-        return ye
-    return k2ye
